f60546708d5604cceb85cc4937e6f7754dc45cd8
kolena <kolena@avast.com>
2018-04-03 15:26:33 +0200

    ScalaDoc improved

--- core/src/main/scala/com/avast/grpc/jsonbridge/GrpcJsonBridge.scala
+++ core/src/main/scala/com/avast/grpc/jsonbridge/GrpcJsonBridge.scala
@@ -6,8 +6,17 @@ import io.grpc.BindableService
 import scala.concurrent.Future
 
 trait GrpcJsonBridge[Service <: BindableService] {
+
+  /** Invokes method with given name, if it exists.
+    *
+    * @param name Name of the method.
+    * @param json Method input (JSON string).
+    * @return None if such method does not exist, Some(Future) otherwise.
+    */
   def invokeGrpcMethod(name: String, json: String, headers: Seq[GrpcHeader] = Seq.empty): Option[Future[String]]
 
+  /** Returns sequence of method names supported by this `GrpcJsonBridge`.
+    */
   def serviceInfo: Seq[String]
 }
 
