f25255e27022e7a2df70c4da64a5ca511a12a90a
Michal Augustyn <augustyn@avast.com>
2019-05-21 16:59:17 +0200

    tests fixed

--- http4s/src/test/scala/com/avast/grpc/jsonbrige/http4s/Http4sTest.scala
+++ http4s/src/test/scala/com/avast/grpc/jsonbrige/http4s/Http4sTest.scala
@@ -1,8 +1,10 @@
 package com.avast.grpc.jsonbrige.http4s
 
+import java.util.concurrent.{Executor, Executors}
+
 import cats.data.NonEmptyList
+import cats.effect.IO
 import com.avast.grpc.jsonbridge._
-import monix.eval.Task
 import org.http4s.headers.{`Content-Length`, `Content-Type`}
 import org.http4s.{Charset, Header, Headers, MediaType, Method, Request, Uri}
 import org.scalatest.FunSuite
@@ -10,28 +12,27 @@ import org.scalatest.concurrent.ScalaFutures
 
 import scala.util.Random
 
-import monix.execution.Scheduler.Implicits.global
-
 class Http4sTest extends FunSuite with ScalaFutures {
 
+  implicit val executor: Executor = Executors.newSingleThreadExecutor()
+
   test("basic") {
-    val service = Http4s(Configuration.Default)(new ReflectionGrpcJsonBridge[Task](TestServiceImpl.bindService()))
+    val service = Http4s(Configuration.Default)(new ReflectionGrpcJsonBridge[IO](TestServiceImpl.bindService()))
 
     val Some(response) = service
       .apply(
-        Request[Task](
+        Request[IO](
           method = Method.POST,
           uri = Uri.fromString("com.avast.grpc.jsonbridge.test.TestService/Add").getOrElse(fail())
         ).withEntity(""" { "a": 1, "b": 2} """)
           .withContentType(`Content-Type`(MediaType.application.json, Charset.`UTF-8`))
       )
       .value
-      .runToFuture
-      .futureValue
+      .unsafeRunSync()
 
     assertResult(org.http4s.Status.Ok)(response.status)
 
-    assertResult("""{"sum":3}""")(response.as[String].runToFuture.futureValue)
+    assertResult("""{"sum":3}""")(response.as[String].unsafeRunSync())
 
     assertResult(
       Headers.of(
@@ -42,21 +43,19 @@ class Http4sTest extends FunSuite with ScalaFutures {
 
   test("path prefix") {
     val configuration = Configuration.Default.copy(pathPrefix = Some(NonEmptyList.of("abc", "def")))
-    val service = Http4s(configuration)(new ReflectionGrpcJsonBridge[Task](TestServiceImpl.bindService()))
+    val service = Http4s(configuration)(new ReflectionGrpcJsonBridge[IO](TestServiceImpl.bindService()))
     val Some(response) = service
       .apply(
-        Request[Task](method = Method.POST,
-                      uri = Uri.fromString("/abc/def/com.avast.grpc.jsonbridge.test.TestService/Add").getOrElse(fail()))
+        Request[IO](method = Method.POST, uri = Uri.fromString("/abc/def/com.avast.grpc.jsonbridge.test.TestService/Add").getOrElse(fail()))
           .withEntity(""" { "a": 1, "b": 2} """)
           .withContentType(`Content-Type`(MediaType.application.json))
       )
       .value
-      .runToFuture
-      .futureValue
+      .unsafeRunSync()
 
     assertResult(org.http4s.Status.Ok)(response.status)
 
-    assertResult("""{"sum":3}""")(response.as[String].runToFuture.futureValue)
+    assertResult("""{"sum":3}""")(response.as[String].unsafeRunSync())
 
     assertResult(
       Headers.of(
@@ -66,18 +65,17 @@ class Http4sTest extends FunSuite with ScalaFutures {
   }
 
   test("bad request after wrong request") {
-    val service = Http4s(Configuration.Default)(new ReflectionGrpcJsonBridge[Task](TestServiceImpl.bindService()))
+    val service = Http4s(Configuration.Default)(new ReflectionGrpcJsonBridge[IO](TestServiceImpl.bindService()))
 
     { // empty body
       val Some(response) = service
         .apply(
-          Request[Task](method = Method.POST, uri = Uri.fromString("com.avast.grpc.jsonbridge.test.TestService/Add").getOrElse(fail()))
+          Request[IO](method = Method.POST, uri = Uri.fromString("com.avast.grpc.jsonbridge.test.TestService/Add").getOrElse(fail()))
             .withEntity("")
             .withContentType(`Content-Type`(MediaType.application.json))
         )
         .value
-        .runToFuture
-        .futureValue
+        .unsafeRunSync()
 
       assertResult(org.http4s.Status.BadRequest)(response.status)
       assertResult("Bad Request")(response.status.reason)
@@ -86,74 +84,70 @@ class Http4sTest extends FunSuite with ScalaFutures {
     {
       val Some(response) = service
         .apply(
-          Request[Task](method = Method.POST, uri = Uri.fromString("com.avast.grpc.jsonbridge.test.TestService/Add").getOrElse(fail()))
+          Request[IO](method = Method.POST, uri = Uri.fromString("com.avast.grpc.jsonbridge.test.TestService/Add").getOrElse(fail()))
             .withEntity(""" { "a": 1, "b": 2} """)
         )
         .value
-        .runToFuture
-        .futureValue
+        .unsafeRunSync()
 
       assertResult(org.http4s.Status.BadRequest)(response.status)
     }
   }
 
   test("propagate user-specified status") {
-    val service = Http4s(Configuration.Default)(new ReflectionGrpcJsonBridge[Task](PermissionDeniedTestServiceImpl.bindService()))
+    val service = Http4s(Configuration.Default)(new ReflectionGrpcJsonBridge[IO](PermissionDeniedTestServiceImpl.bindService()))
 
     val Some(response) = service
       .apply(
-        Request[Task](method = Method.POST, uri = Uri.fromString("com.avast.grpc.jsonbridge.test.TestService/Add").getOrElse(fail()))
+        Request[IO](method = Method.POST, uri = Uri.fromString("com.avast.grpc.jsonbridge.test.TestService/Add").getOrElse(fail()))
           .withEntity(""" { "a": 1, "b": 2} """)
           .withContentType(`Content-Type`(MediaType.application.json))
       )
       .value
-      .runToFuture
-      .futureValue
+      .unsafeRunSync()
 
     assertResult(org.http4s.Status.Forbidden)(response.status)
     assertResult("Forbidden")(response.status.reason)
   }
 
   test("provides service info") {
-    val service = Http4s(Configuration.Default)(new ReflectionGrpcJsonBridge[Task](TestServiceImpl.bindService()))
+    val service = Http4s(Configuration.Default)(new ReflectionGrpcJsonBridge[IO](TestServiceImpl.bindService()))
 
     val Some(response) = service
       .apply(
-        Request[Task](method = Method.GET, uri = Uri.fromString("com.avast.grpc.jsonbridge.test.TestService").getOrElse(fail()))
+        Request[IO](method = Method.GET, uri = Uri.fromString("com.avast.grpc.jsonbridge.test.TestService").getOrElse(fail()))
       )
       .value
-      .runToFuture
-      .futureValue
+      .unsafeRunSync()
 
     assertResult(org.http4s.Status.Ok)(response.status)
 
-    assertResult("com.avast.grpc.jsonbridge.test.TestService/Add")(response.as[String].runToFuture.futureValue)
+    assertResult("com.avast.grpc.jsonbridge.test.TestService/Add")(response.as[String].unsafeRunSync())
   }
 
   test("provides services info") {
-    val service = Http4s(Configuration.Default)(new ReflectionGrpcJsonBridge[Task](TestServiceImpl.bindService()))
+    val service = Http4s(Configuration.Default)(new ReflectionGrpcJsonBridge[IO](TestServiceImpl.bindService()))
 
     val Some(response) = service
       .apply(
-        Request[Task](method = Method.GET, uri = Uri.fromString("/").getOrElse(fail()))
+        Request[IO](method = Method.GET, uri = Uri.fromString("/").getOrElse(fail()))
       )
       .value
-      .runToFuture
-      .futureValue
+      .unsafeRunSync()
 
     assertResult(org.http4s.Status.Ok)(response.status)
 
-    assertResult("com.avast.grpc.jsonbridge.test.TestService/Add")(response.as[String].runToFuture.futureValue)
+    assertResult("com.avast.grpc.jsonbridge.test.TestService/Add")(response.as[String].unsafeRunSync())
   }
 
   test("passes user headers") {
-    val service = Http4s(Configuration.Default)(new ReflectionGrpcJsonBridge[Task](TestServiceImpl.withInterceptor))
+    val service = Http4s(Configuration.Default)(new ReflectionGrpcJsonBridge[IO](TestServiceImpl.withInterceptor))
 
     val headerValue = Random.alphanumeric.take(10).mkString("")
 
     val Some(response) = service
       .apply(
-        Request[Task](
+        Request[IO](
           method = Method.POST,
           uri = Uri.fromString("com.avast.grpc.jsonbridge.test.TestService/Add").getOrElse(fail()),
           headers = Headers.of(Header(TestServiceImpl.HeaderName, headerValue))
@@ -161,8 +155,7 @@ class Http4sTest extends FunSuite with ScalaFutures {
           .withContentType(`Content-Type`(MediaType.application.json))
       )
       .value
-      .runToFuture
-      .futureValue
+      .unsafeRunSync()
 
     assertResult(org.http4s.Status.Ok)(response.status)
     assertResult(headerValue)(TestServiceImpl.lastContextValue.get())
