90709f95dcc6c33970b0d9af58b9684a6a9182da
Ondra Pelech <ondra.pelech@gmail.com>
2019-08-16 18:01:26 +0200

    remove unnecessary extending in `BridgeError.Narrow`

--- core/src/main/scala/com/avast/grpc/jsonbridge/BridgeError.scala
+++ core/src/main/scala/com/avast/grpc/jsonbridge/BridgeError.scala
@@ -7,7 +7,7 @@ sealed trait BridgeError extends Exception with Product with Serializable
 object BridgeError {
   final case object GrpcMethodNotFound extends BridgeError
 
-  sealed trait Narrow extends BridgeError with Product with Serializable
+  sealed trait Narrow extends BridgeError
   final case class RequestJsonParseError(t: InvalidProtocolBufferException) extends Narrow
   final case class RequestErrorGrpc(s: Status) extends Narrow
   final case class RequestError(t: Throwable) extends Narrow
